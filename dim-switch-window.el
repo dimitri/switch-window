2991ede68df3db974b2405835686a5dee445c0d6
Dimitri Fontaine <dim@DimIMac.local>
2010-04-30 21:16:42 +0200

    Finish dim:switch-window which must avoid text-scale-adjust.

--- dim-switch-window.el
+++ dim-switch-window.el
@@ -1,18 +1,27 @@
-(defun my-switch-window-display-number (win num)
+;;; dim-switch-window.el
+;;
+;; Offer a *visual* way to choose a window to switch to
+
+(defgroup dim:switch-window nil "dim:switch-window customization group"
+  :group 'convenience)
+
+(defcustom dim:switch-window-increase 12
+  "How much to increase text size in the window numbering"
+  :type 'integer
+  :group 'dim:switch-window)
+
+(defun dim:switch-window-display-number (win num)
   "prepare a temp buffer to diplay in the window while choosing"
   (let ((buf (get-buffer-create
 	      (concat " *" (number-to-string num) "*"))))
     (with-current-buffer buf
       (insert (concat "\n\n    " (number-to-string num)))
-      (text-scale-adjust 12))
+      (text-scale-increase dim:switch-window-increase))
 
     (set-window-buffer win buf)
-    (select-window win)
-    (switch-to-buffer buf 'non-record)
-    (sit-for 0)
     buf))
 
-(defun my-switch-window ()
+(defun dim:switch-window ()
   "Display an overlay in each window showing a unique key, then
 ask user for the window where move to"
   (interactive)
@@ -22,24 +31,29 @@ ask user for the window where move to"
 	key
         buffers)
     
+    ;; display big numbers to ease window selection
     (dolist (win (window-list))
-      (setq buffers (cons (my-switch-window-display-number win num) buffers))
+      (setq buffers (cons (dim:switch-window-display-number win num) buffers))
       (setq num (1+ num)))
 
-    (redisplay 1)
-    (sit-for 1)
+    ;; choose a window
     (while (not key)
       (let ((input (event-basic-type (read-event "Move to window: " nil 5))))
 	(when (and (not (symbolp input)) (<= 49 input) (>= 57 input))
 	  (setq key (- input 48)))))
-    (message "%S" key)
 
+    ;; get those huge numbers away
     (dolist (buf buffers)
-      (kill-buffer buf)
-      (set-window-configuration config))
+      (kill-buffer buf))
 
+    (set-window-configuration config)
+
+    ;; move to selected window
     (setq num 1)
     (dolist (win (window-list))
       (when (eq num key)
 	(select-window win))
       (setq num (1+ num)))))
+
+(global-set-key (kbd "C-x o") 'dim:switch-window)
+(provide 'dim-switch-window)
